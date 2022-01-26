cab5fc86b9a527f345654d812c4f79f1b9b8b4ad
Evgenii <melegarifulin@gmail.com>
2022-01-26 17:06:13 +0500

    0.6
    Fully adaptive. Bug fixes.

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,6 @@
 import { carousel } from '../libs/bootstrap/bootstrap.bundle.min.js';
 import { sliderPhotoActivate } from './sliderPhoto'
+import { mobileMenuShow } from './mobileMenuShow'
 
+mobileMenuShow ();
 sliderPhotoActivate ();
---
+++ app/js/mobileMenuShow.js
@@ -0,0 +1,34 @@
+const mobileMenuShow = () => {
+	const menu = document.querySelector('.header__mobile');
+	const body = document.querySelector('body');
+	const overlay = document.querySelector('.page-overlay');
+	const openButton = document.querySelector('.header__burger');
+	const closeButton = document.querySelector('.header__close');
+
+	openButton.addEventListener('click', () => {
+		body.classList.add('no-scroll');
+		menu.classList.add('header__mobile--active');
+		overlay.classList.add('page-overlay--active');
+		closeButton.classList.add('header__close--active');
+		openButton.classList.remove('header__burger--active');
+	});
+
+	closeButton.addEventListener('click', () => {
+		body.classList.remove('no-scroll');
+		menu.classList.remove('header__mobile--active');
+		overlay.classList.remove('page-overlay--active');
+		closeButton.classList.remove('header__close--active');
+		openButton.classList.add('header__burger--active');
+	});
+
+	overlay.addEventListener('click', () => {
+		body.classList.remove('no-scroll');
+		menu.classList.remove('header__mobile--active');
+		overlay.classList.remove('page-overlay--active');
+		closeButton.classList.remove('header__close--active');
+		openButton.classList.add('header__burger--active');
+	});
+	console.log(overlay);
+};
+
+export { mobileMenuShow };
